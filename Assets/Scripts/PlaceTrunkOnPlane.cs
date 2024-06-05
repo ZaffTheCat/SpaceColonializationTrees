8fd80c8171677b0cf773cce5fc40171c4f2739d6
ZaffTheCat <barbara.bensari@gmail.com>
2024-06-05 14:20:22 +0200

    Create a ray from camera to plane

---
+++ Assets/Scripts/PlaceTrunkOnPlane.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTrunkOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    public new Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // LMB pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 1000.0f);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Debug.Log("Hit");
+            }
+
+        }
+        
+    }
+}
