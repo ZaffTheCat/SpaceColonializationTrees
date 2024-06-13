5dcfaa963e9fbf1a75789ae81240e7ee616e6530
ZaffTheCat <barbara.bensari@gmail.com>
2024-06-13 16:43:50 +0200

    adding branches

--- Assets/Scripts/AttractorSpawnerScript.cs
+++ Assets/Scripts/AttractorSpawnerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class AttractorSpawner : MonoBehaviour
 {
     public GameObject _attractorObject;
+    public GameObject _branchObject;
     public BranchManager _branchManager;
 
     [Header("Attractor Spawn Settings")]
@@ -14,10 +15,10 @@ public class AttractorSpawner : MonoBehaviour
     private static int attractorCount;
 
     [Header("Attraction Distance Settings")]
-    private static float minimumDistance = 2.7f;
-    private static float cullDistance = 0.9f;
+    private static float minimumDistance = 4.5f;
+    private static float cullDistance = 2.0f;
 
-    private List<GameObject> spawns = new List<GameObject>();
+    private List<GameObject> attractorSpawns = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -52,36 +53,46 @@ public class AttractorSpawner : MonoBehaviour
         for (int i = 0; i < attractorCount; i++)
         {
             attractorPlacement = new Vector3(attractorOrigin.x + Random.Range(-attractorRange, attractorRange), attractorOrigin.y + Random.Range(0, attractorRange), attractorOrigin.z + Random.Range(-attractorRange, attractorRange));
-            spawns.Add(Instantiate(_attractorObject, attractorPlacement, Quaternion.identity));
+            attractorSpawns.Add(Instantiate(_attractorObject, attractorPlacement, Quaternion.identity));
         }
     }
 
     public void Populate()
     {
         //iterate through each branch
-        for(int i =0; i <_branchManager._branchList.Count; i++)
+        for(int i = 0; i <_branchManager._branchList.Count; i++)
         {
             //iterate through each attractor
-            for(int j=0; j< spawns.Count; j++)
+            for(int j=0; j< attractorSpawns.Count; j++)
             {
                 //calculate the distance between branch and attratcor
-                float distance = Vector3.Distance(_branchManager._branchList[i].transform.position, spawns[j].transform.position);
-                Debug.Log(distance);
+                float distance = Vector3.Distance(_branchManager._branchList[i].transform.position, attractorSpawns[j].transform.position);
+                
 
                 //delete attractots that are too close
                 if (distance < cullDistance)
                 {
-                    spawns.RemoveAt(j);
+                    Destroy(attractorSpawns[j]);
+                    attractorSpawns.RemoveAt(j);
                     continue;
                 }
                 //create branch in the direction of the attractor if in range
                 else if (distance < minimumDistance) {
-
+                    Vector3 direction = (attractorSpawns[j].transform.position - _branchManager._branchList[i].transform.position).normalized;
+                    Vector3 position = _branchManager._branchList[i].transform.position + direction;
+                    Quaternion quaternionDirection = Quaternion.identity;//Quaternion.LookRotation(direction, Vector3.up);
+                    _branchManager.AddBranch((Instantiate(_branchObject, position, quaternionDirection)));
                 }
 
             }
         }
-        //empty the list at the end
+        //empty the list at the end and delete unused attractors
         _branchManager._branchList.Clear();
+
+        for (int i = 0; i < attractorSpawns.Count; i++)
+        {
+            Destroy(attractorSpawns[i]);
+        }
+        attractorSpawns.Clear();
     }
 }
